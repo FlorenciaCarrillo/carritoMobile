cad2cde15a5c8759c0385a0f54f042a0b5b873d2
florencia <florencianoelcarrillo@gmail.com>
2024-09-03 22:19:46 -0300

    Se crean Main, Home e Intent

--- app/src/main/java/com/example/ejemplocarrito/HomeActivity.java
+++ app/src/main/java/com/example/ejemplocarrito/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.ejemplocarrito;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button botonCliente, botonAdmin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,29 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        botonCliente = (Button)findViewById(R.id.btnCliente);
+        botonAdmin = (Button)findViewById(R.id.btnAdmin);
+
+        botonCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        botonAdmin.setOnClickListener(new View.OnClickListener()
+
+            {
+                @Override
+                public void onClick (View view){
+                    Intent intent = new Intent(HomeActivity.this, LoginAdminActivity.class);
+                    startActivity(intent);
+            }
+            });
+        }
+
     }
-}
+
